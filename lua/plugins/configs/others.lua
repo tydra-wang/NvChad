fd611e3c7df7a47d30bb8a9e8549eb41270626ba
wangjunjie <wangjunjie@datopia.cn>
2021-12-30 15:59:38 +0800

    feat: improve gitsigns signcolume for monokai font in windows

--- lua/plugins/configs/others.lua
+++ lua/plugins/configs/others.lua
@@ -151,9 +151,9 @@ M.gitsigns = function()
    if present then
       gitsigns.setup {
          signs = {
-            add = { hl = "DiffAdd", text = "│", numhl = "GitSignsAddNr" },
-            change = { hl = "DiffChange", text = "│", numhl = "GitSignsChangeNr" },
-            delete = { hl = "DiffDelete", text = "", numhl = "GitSignsDeleteNr" },
+            add = { hl = "DiffAdd", text = "+", numhl = "GitSignsAddNr" },
+            change = { hl = "DiffChange", text = "+", numhl = "GitSignsChangeNr" },
+            delete = { hl = "DiffDelete", text = "_", numhl = "GitSignsDeleteNr" },
             topdelete = { hl = "DiffDelete", text = "‾", numhl = "GitSignsDeleteNr" },
             changedelete = { hl = "DiffChangeDelete", text = "~", numhl = "GitSignsChangeNr" },
          },
